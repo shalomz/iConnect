821324fd8a565e29ab02bd0281efd9005f3078af
shalomz <shalom.nyende@gmail.com>
2017-04-23 13:21:28 +0300

    Initial Applications for the Project.

--- src/account/models.py
+++ src/account/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
+import datetime
+from django.core.cache import cache
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    date_of_birth = models.DateField(blank=True, null=True)
+    joined = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+
+    def last_seen(self):
+        return cache.get('seen_%s' % self.user.username)
+
+    def online(self):
+        if self.last_seen():
+            now = datetime.datetime.now()
+            if now > self.last_seen() + datetime.timedelta(
+                         seconds=settings.USER_ONLINE_TIMEOUT):
+                return False
+            else:
+                return True
+        else:
+            return False
+
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, releated_name='posts_created')
+    content = models.CharField(max_length=140)
+    created_time = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+    liked = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='liked')
+
+    class Meta:
+        ordering = ('-created_time',)
+
+    def __str__(self):
+        return self.content
+
+
+class HashTag(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    post = models.ManyToManyField(Post)
+
+    def __str__(self):
+        return self.name
+
